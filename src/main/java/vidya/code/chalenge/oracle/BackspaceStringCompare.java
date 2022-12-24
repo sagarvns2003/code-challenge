4515d93a269e74b1c11e1015c217f0791b2ef156
Vidya Sagar Gupta <sagarvns2003@gmail.com>
2022-12-24 23:30:02 +0530

    Oracle: Backspace String Compare

---
+++ src/main/java/vidya/code/chalenge/oracle/BackspaceStringCompare.java
@@ -0,0 +1,48 @@
+package vidya.code.chalenge.oracle;
+
+/*
+ * Given two strings s and t, return true if they are equal when both are typed into empty text editors. 
+ * hash '#' means a backspace character.
+ * 
+ * Note that after backspacing an empty text, the text will continue empty.
+ * 
+ * Example:
+       Input: s = "ab#c", t = "ad#c"
+       Output: true
+       Explanation: Both s and t become "ac".
+ *
+ */
+public class BackspaceStringCompare {
+
+	public static void main(String[] args) {
+
+		//input strings
+		String s = "#ab#c", t = "ad#c";
+
+		System.out.println("Is both strings are same ? " + compare(s, t));
+	}
+
+	private static boolean compare(String s, String t) {
+		s = prepareString(s);
+		t = prepareString(t);
+
+		System.out.println("Final string: " + s);
+		System.out.println("Final string: " + t);
+
+		return s.equals(t);
+	}
+
+	private static String prepareString(String string) {
+		StringBuilder sb = new StringBuilder();
+		for (char ch : string.toCharArray()) {
+			if (ch == '#' && sb.length() != 0) {
+				sb.deleteCharAt(sb.length() - 1);
+			} else {
+				if (ch != '#') {
+					sb.append(ch);
+				}
+			}
+		}
+		return sb.toString();
+	}
+}
